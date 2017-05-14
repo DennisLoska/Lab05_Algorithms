58c2fa6fa716c816d14bfefe4fe58933f7fc434d
DennisTalmid <d.loska@hotmail.de>
2017-05-14 14:29:58 +0200

    finishing final java class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Main {
 
@@ -7,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-
 
         System.out.println("Case n=10:");
         main.fragment01(n10);
@@ -38,7 +38,18 @@ public class Main {
         main.fragment06(n1000);
         main.fragment07(n1000);
 
-        System.out.println(main.isPrime(8));
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 100; i++) {
+            long randLong = main.randomBinaryGenerator(40);
+            main.isPrime3(randLong);
+            //System.out.println(randDouble + " is prime? --> " + main.isPrime3(randDouble));
+        }
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Operations took " + duration + "ms");
+        //main.randomBinaryGenerator(40);
     }
 
 	/*
@@ -110,7 +121,7 @@ public class Main {
         System.out.println("Fragment 6 sum is: " + sum);
     }
 
-    //O(N^3) kubische Komplexität
+    //O(N^4) kubische Komplexität
     public void fragment07(int n) {
         int sum = 0;
         for (int i = 1; i < n; i++) {
@@ -129,7 +140,52 @@ public class Main {
      * * PART 2: Prime Numbers
      */
 
-    public boolean isPrime(int n) {
+    public boolean isPrime(double n) {
+        // WORST CASE: O(n)
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum++;
+            }
+        }
+//		System.out.println(sum);
+//		String b = Integer.toBinaryString(n);
+//		System.out.println(n + " --> to binary String --> " + b);
+        if (!(sum == 2)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isPrime2(double n){
+        for (int i = 2; 2*i < n; i++){
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    boolean isPrime3(double n) {
+        //check if n is a multiple of 2
+        if (n%2==0) return false;
+        //if not, then just check the odds
+        for(int i=3;i*i<=n;i+=2) {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    boolean isPrime4(long n) {
+        for(int i=2;i<Math.sqrt(n);i++) {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isPrime5(double n) {
         if (n <= 2)
             return (n == 2);
         for (int i = 2; i < n; i++) {
@@ -138,5 +194,26 @@ public class Main {
         }
         return true;
     }
+
+    public long randomBinaryGenerator(int bitCount) {
+        Random rand = new Random();
+        StringBuilder binBuilder = new StringBuilder();
+        String randBinDigit = "";
+        String bin = "";
+        long bin2int;
+
+        // create the binary String bin
+        for (int i = 0; i < bitCount; i++) {
+            randBinDigit = String.valueOf(rand.nextInt(2));
+            binBuilder.append(randBinDigit);
+        }
+        bin = binBuilder.toString();
+
+        // convert binary String to double number
+        bin2int = Long.parseLong(bin, 2);
+        return bin2int;
+    }
+
+
 
 }
